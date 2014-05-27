5762360e418dc0260958b5c1a9d2627623b76fbd
Koki-Igarashi <igarashi.koki@gmail.com>
2014-05-27 18:34:27 +0900

    first_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keiziban3::Application.config.secret_token = '56aa6b3ff1c6b0df2629de3c828373c64e273bc80114275e658bf99c89a382669878efcc4c3df5a7e4540931ce872c9510808cae84d9fa8a217fa34a5a6065a9'
